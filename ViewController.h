6ab871956fe0b8eab04ec718f583ddb07dd48ec7
divyasai68 <divyasai68@gmail.com>
2016-07-14 22:59:50 -0700

    First commit
    Facebook SDK in to app

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewApplication
+//
+//  Created by Divya sai Mekala on 7/14/16.
+//  Copyright © 2016 Divya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewApplication
+//
+//  Created by Divya sai Mekala on 7/14/16.
+//  Copyright © 2016 Divya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
